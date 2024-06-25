fa1320f00082b5df68c0648e931bd173314880e5
Matt Hogg <matt.hogg74@hotmail.com>
2024-06-25 18:50:37 +0100

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LQSONSwift",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "LQSONSwift",
+            targets: ["LQSONSwift"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "LQSONSwift"),
+        .testTarget(
+            name: "LQSONSwiftTests",
+            dependencies: ["LQSONSwift"]),
+    ]
+)
